3b8211409916bcba085c0dd0d808ab45fa6c40d6
unai <unaicc2003@gmail.com>
2023-10-23 12:12:19 +0200

    0909

--- api/models.py
+++ api/models.py
@@ -5,10 +5,10 @@ class Examen(models.Model):
     asignature = models.CharField(max_length=90)
     date = models.TextField(max_length=11)
     def __str__(self) -> str:
-        return self.asignature
+        return self.asignature +self.date
 class Apunte(models.Model):
     Asignatura = models.CharField(max_length=40)
     tema = models.CharField(max_length=4)
     text = models.TextField(max_length=10000)
     def __str__(self) -> str:
-        return self.Asignatura
+        return self.Asignatura+ self.tema
